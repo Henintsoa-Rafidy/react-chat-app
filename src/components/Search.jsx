40fba7a48984f43211aee955c4b4799239b417d9
henintsoa_rafidy <henintsoa.rafidy@gmail.com>
2023-01-11 13:05:29 +0100

    Début recherche

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,48 @@
 import React from 'react'
-import Pokeball from '../img/pokeball.png'
+import { useState } from 'react'
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from '../firebase';
 
 const Search = () => {
+
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+  
+  const handleSearch = async () => {
+    const q = query(collection(db, "users"), where("displayName", "==", username));
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      });
+    } catch (error) {
+      setErr(true);
+      console.log(error)      
+    }
+  };
+
+  const handleKey = e => {
+    e.code === "Enter" && handleSearch()
+  }
+
+
+
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='find a user' name="" id="" />
+        <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)}/>
       </div>
-      <div className="userChat">
-        <img src={Pokeball} alt="" />
+     
+     {user && <div className="userChat">
+        <img src={user.photoURL} alt="" />
         <div className="userChatInfo">
-          <span className="jane">Jane</span>
+          <span className="jane">{user.displayName}</span>
         </div>
-      </div>
+      </div>}
+      
+      {err === true && <p>An error occurred</p>}
     </div>
   )
 }
